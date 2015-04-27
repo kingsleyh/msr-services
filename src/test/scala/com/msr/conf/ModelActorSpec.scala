d2776537d03b67594a9f45a1a8259ff9804d1d30
mamalisk <mamalis.kostas@gmail.com>
2015-04-27 20:21:48 +0100

    adding specs2 tests

---
+++ src/test/scala/com/msr/conf/ModelActorSpec.scala
@@ -0,0 +1,41 @@
+package com.msr.conf
+
+import org.scalatest.{ FlatSpecLike, BeforeAndAfterAll }
+import akka.testkit.{ TestActorRef, ImplicitSender, TestKit }
+import akka.actor.{ Props, ActorSystem }
+
+class ModelActorSpec extends TestKit(ActorSystem()) with FlatSpecLike with ImplicitSender with BeforeAndAfterAll {
+
+  import ModelActor._
+
+  override def afterAll() {
+    system.shutdown()
+  }
+
+  val model = TestActorRef(Props[ModelActor])
+
+  "A Model" should "return a list of 5 ItemSummaries" in {
+    model ! 'list
+    val lst = expectMsgType[ItemSummaries]
+    assert(lst.items.size === 5)
+  }
+
+  it should "return 3 items containing 'Qu'" in {
+    model ! ('query, "Qu")
+    val lst = expectMsgType[ItemSummaries]
+    assert(lst.items.size === 3)
+  }
+
+  it should "return item 1 when asked" in {
+    model ! 1
+    val item = expectMsgType[Item]
+    assert(item.id === 1)
+    assert(item.title === "foo")
+  }
+
+  it should "return ItemNotFound when requested item is not found" in {
+    model ! 10
+    expectMsg(ItemNotFound)
+  }
+
+}
---
+++ src/test/scala/com/msr/conf/ModelSpec.scala
@@ -0,0 +1,26 @@
+package com.msr.conf
+
+import org.scalatest.FlatSpec
+
+class ModelSpec extends FlatSpec {
+
+  "A Model" should "return a list of 5 ItemSummaries" in new Model {
+    assert(list.size === 5)
+  }
+
+  it should "return 3 items containing 'Qu'" in new Model {
+    assert(query("Qu").size === 3)
+  }
+
+  it should "return item 1 when asked" in new Model {
+    val item = get(1).get
+    assert(item.id === 1)
+    assert(item.title === "foo")
+    assert(item.stock === 2)
+  }
+
+  it should "return None when requested item is not found" in new Model {
+    assert(get(100) === None)
+  }
+
+}
